864f3919784aa1d2f61eee47313da3f0d334bd77
Herman Chan <hermchan89@gmail.com>
2016-04-11 15:38:24 -0700

    controllers able to edit saved data and save to database

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -20,9 +20,9 @@ function create(req, res) {
   var newMessage = new db.Message ({
     user: req.body.user,
     dateSent: '132323232',
-    bodyText: req.body.body_text,
+    bodyText: req.body.bodyText,
     from: req.body.from,
-    recipients: [{ name: "Kyle", number: req.body.to,}]
+    recipients: [{ name: req.body.to, number: req.body.number,}]
   });
 
   newMessage.save(function(err, message){
@@ -43,20 +43,30 @@ function show(req, res) {
 function destroy(req, res) {
   // FILL ME IN
   db.Message.findByIdAndRemove({ _id: req.params.messageId }, function(err, foundMessage){
-    console.log('successfully removed', req.params.messageId);
-    res.json(foundMessage);
+    console.log('successfully removed id:', req.params.messageId);
+    res.send(foundMessage);
   });
 }
 
 function update(req, res) {
   // FILL ME IN !
+  console.log('updating id ', req.params.messageId);
+  console.log('received body ', req.body);
+
   db.Message.findById({ _id: req.params.messageId}, function(err, foundMessage) {
     if(err) {
       console.log('messageController.update error', err);
     }
-    console.log(foundMessage.user);
+    console.log('found message', foundMessage);
     foundMessage.user = req.body.user;
-    foundMessage.bodyText = req.body.body_text;
+    console.log('found message user', req.body.user);
+
+    foundMessage.bodyText = req.body.bodyText;
+    console.log('found body text', req.body.bodyText);
+
+    foundMessage.recipients[0].number = req.body.recipients[0].number;
+    console.log('found recipients number', req.body.recipients[0].number);
+
       foundMessage.save(function(err, savedUpdate) {
         if (err) {
           console.log('Saved Update Error: ', err);
