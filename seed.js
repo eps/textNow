436c0c20d5da401e95c020fd6fabee94b52784b0
Herman Chan <hermchan89@gmail.com>
2016-04-10 18:24:39 -0700

    removed all unused code in app.js, edited handlebars

--- seed.js
+++ seed.js
@@ -2,28 +2,32 @@ var db = require("./models");
 
 var messageList = [
 {
-   dateSent: 'June 28, 2016',
-   bodyText: 'Hi, what are you doing today?',
-   from: '+14152134815',
-   recipients: [{ name: 'Bob', number: '+14152931940'}, { name: 'Steve', number: '+14158501895'}]
+ user: 'Andy',
+ dateSent: 'June 28, 2016',
+ bodyText: 'Hi, what are you doing today?',
+ from: '+14152134815',
+ recipients: [{ name: 'Bob', number: '+14152931940'}, { name: 'Carol', number: '+14158501895'}]
 },
 {
-   dateSent: 'July 22, 2016',
-   bodyText: 'Where are you?',
-   from: '+14153049149',
-   recipients: [{ name: 'Steve', number: '+14158501895'}]
+ user: 'John',
+ dateSent: 'July 22, 2016',
+ bodyText: 'Where are you?',
+ from: '+14153049149',
+ recipients: [{ name: 'Steve', number: '+14158501895'}]
 },
 {
-   dateSent: 'November 30, 2016',
-   bodyText: 'HOLY SHIT! what is that?',
-   from: '14158124749',
-   recipients: [{ name: 'Hannah', number: '+14152159149'}]
- },
+ user: 'Sam',
+ dateSent: 'November 30, 2016',
+ bodyText: 'HOLY SHIT! what is that?',
+ from: '14158124749',
+ recipients: [{ name: 'Hannah', number: '+14152159149'}]
+},
 {
-   dateSent: 'March 17, 2016',
-   bodyText: 'Good morning all',
-   from: '+14156472937',
-   recipients: [{ name: 'Kyle', number: '+14152931940'}]
+ user: 'Valerie',
+ dateSent: 'March 17, 2016',
+ bodyText: 'Good morning all',
+ from: '+14156472937',
+ recipients: [{ name: 'Kyle', number: '+14152931940'}]
 }];
 
 
